58d2f60c856a53cae5ebab26c3d49ce7fa645ffa
Vincent <2w121q@gmail.com>
2023-11-02 10:35:36 +0800

    Add the playRound function

--- javascript.js
+++ javascript.js
@@ -14,4 +14,18 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function playRound(p, cS) {
+    let pS = p.toLowerCase().replace(p[0], p[0].toUpperCase());
+
+    if (pS === cS) {
+        return "It's a tie";
+    } else if (pS === 'Rock' && cS === 'Scissors' || pS === 'Paper' && cS === 'Rock' || pS === 'Scissors' && cS === 'Paper') {
+        return `You win! ${pS} beats ${cS}`;
+    } else {
+        return `You lose! ${cS} beats ${pS}`;
+    }
+}
+
+let x = getComputerChoice();
+console.log(x);
+console.log(playRound('rock', x));
